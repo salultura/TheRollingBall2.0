915e8e452d0152e36a2ddfa12599d9e9512abfd8
Salustiano Oliveira <salultura@gmail.com>
2018-03-21 12:15:42 -0300

    Dando vida ao jogo
    - Adicionado script para a monimentação do personagem;
    - Adicionado script para a movimentação da câmera;
    - Adicionado textura ao personagem;
    - Modificando propriedades da luz do jogo;
    - Modificando propriedades da câmera do jogo;

---
+++ The Rolling Ball 2.0/Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour {
+    public GameObject jogador;
+    private Vector3 distanciaParaJogador;
+	// Use this for initialization
+	void Start () {
+        distanciaParaJogador = gameObject.transform.position - jogador.transform.position;
+	}
+
+    void LateUpdate()
+    {
+        gameObject.transform.position = distanciaParaJogador + jogador.transform.position;    
+    }
+}
---
+++ The Rolling Ball 2.0/Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public float velocidade = 8;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovimentacaoDoPersonagem();
+    }
+
+    private void MovimentacaoDoPersonagem()
+    {
+        float movimentoX = Input.GetAxis("Horizontal");
+        float movimentoZ = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(movimentoX, 0.0f, movimentoZ);
+
+        rb.AddForce(direcao * velocidade);
+    }
+}
